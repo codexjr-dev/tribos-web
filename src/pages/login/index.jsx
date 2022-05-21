f9f3225b07682ec7dfa2b821ca752cc1af3fe065
Luciano Erick Figueiredo <luciano.erick.filho@ccc.ufcg.edu.br>
2022-05-21 17:31:37 -0300

    Modulando estilos

---
+++ src/pages/login/index.jsx
@@ -0,0 +1,20 @@
+import styles from "./styles.module.css";
+
+/*
+    Esse padrão de uso de css se chama css modules
+    
+    ** É importante usar pra não dar choque de estilos no css
+    em diferentes arquivos.
+
+    Basicamente é assim o uso
+*/
+
+const Login = () => {
+    return (
+        <div className={styles.container}>
+            Essa é a tela de login.
+        </div>
+    )
+
+}
+export default Login;
