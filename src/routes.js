56216acb84df999abbb66d0f20e29fc55aa1a362
matheusforlan <matheus.andrade@ccc.ufcg.edu.br>
2022-06-02 18:54:27 -0300

    Adicionando estilização do login

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,9 @@ import { Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/dashboard";
 import Details from "./pages/details";
 import Login from "./pages/login";
+import Feedback from "./pages/feedback";
+import Errors from "./pages/errors";
+import Spam from "./pages/spam";
 
 export default function MainRoutes() {
   const isLogged = true;
@@ -12,6 +15,9 @@ export default function MainRoutes() {
       <Route path="/login" element={<Login />} />
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/detalhes" element={<Details />} />
+      <Route path="/feedback" element={<Feedback />} />
+      <Route path="/errors" element={<Errors />} />
+      <Route path="/spam" element={<Spam />} />
     </Routes>
   );
 }
