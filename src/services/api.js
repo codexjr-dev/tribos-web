9ae619b854ade1f2ad96ce8f43922d90e557bde8
Luciano Erick Figueiredo <luciano.erick.filho@ccc.ufcg.edu.br>
2022-07-15 14:53:43 -0300

    Adicionando método GET para obter estatisticas

--- src/services/api.js
+++ src/services/api.js
@@ -4,21 +4,16 @@ export const api = axios.create({
   baseURL: "https://tribos-project.herokuapp.com",
 });
 
-// export const getStatisticsByDate = async () => {
-//   let data = null;
-//   try {
-//     await api
-//       .get(`/statistics/${"users"}`, {
-//         Authorization: `Basic ${localStorage.getItem("@App:token")}`,
-//       })
-//       .then((res) => {
-//         data = res.data;
-//       });
-//   } catch (e) {
-//     console.log(e);
-//   }
-//   return data;
-// };
+export const getStatiticsByDate = async (type, beginDate, finalDate) => {
+  let data;
+  await api
+    .get(`/statistics/${type}/${beginDate}${finalDate ? `/${finalDate}` : ""}`)
+    .then((res) => {
+      data = res.data;
+    })
+    .catch((e) => console.log(e));
+  return data;
+};
 
 export const getAllReports = async () => {
   let data = null;
