f0b57e2e11e1cef5be095f112d1085cee5b53c0a
Luciano Erick Figueiredo <luciano.erick.filho@ccc.ufcg.edu.br>
2022-07-15 14:56:05 -0300

    Adicionando funcionalidades auxiliares

--- src/util/aux.js
+++ src/util/aux.js
@@ -30,15 +30,3 @@ export const formatInfo = (value) => {
   }
   return output;
 };
-
-export const typeOptions = [
-  { label: "Usuários", value: "users" },
-  { label: "Anúncios", value: "ads" },
-  { label: "Tribos", value: "groups" },
-];
-
-export const intervalOptions = [
-  { label: "Dia", value: "day" },
-  { label: "Mês", value: "month" },
-  { label: "Ano", value: "year" },
-];
