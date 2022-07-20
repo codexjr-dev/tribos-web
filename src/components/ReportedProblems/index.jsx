0fc325c8caa2df8c1116d95ccc2c7d39aa92022d
Luciano Erick Figueiredo <luciano.erick.filho@ccc.ufcg.edu.br>
2022-07-20 08:52:17 -0300

    Criando componente NavigationButton

--- src/components/ReportedProblems/index.jsx
+++ src/components/ReportedProblems/index.jsx
@@ -1,37 +1,19 @@
 import reportIcon from "../../assets/icons/report-icon.svg";
 import messageIcon from "../../assets/icons/message-icon.svg";
 import errorIcon from "../../assets/icons/error-icon.svg";
-import rightArrowIcon from "../../assets/icons/right-arrow-icon.svg";
 
 import styles from "./styles.module.css";
+import { NavigateButton } from "../NavigateButton";
 
 const ReportedProblems = () => {
   return (
     <nav className={styles.container}>
       <h3>Problemas Reportados</h3>
-      <ul>
-        <li>
-          <img src={reportIcon} alt="Report" />
-          <div id={styles.icon}>
-            <p>Spam ou Abusos</p>
-            <img src={rightArrowIcon} alt="Report" />
-          </div>
-        </li>
-        <li>
-          <img src={messageIcon} alt="Feedback" />
-          <div id={styles.icon}>
-            <p>Feedbacks Gerais</p>
-            <img src={rightArrowIcon} alt="Report" />
-          </div>
-        </li>
-        <li>
-          <img src={errorIcon} alt="Error" />
-          <div id={styles.icon}>
-            <p>Erros do Sistema</p>
-            <img src={rightArrowIcon} alt="Report" />
-          </div>
-        </li>
-      </ul>
+      <div className={styles.listContainer}>
+        <NavigateButton name="Spam ou abusos" srcIcon={reportIcon} navigateTo="spam"/>
+        <NavigateButton name="Feedback" srcIcon={messageIcon} navigateTo="feedback"/>
+        <NavigateButton name="Erros do Sistema" srcIcon={errorIcon} navigateTo="error"/>
+      </div>
     </nav>
   );
 };
