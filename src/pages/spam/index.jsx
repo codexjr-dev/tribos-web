ac797a46d3df7c2822dd617cde84256c99a1f27f
matheusforlan <matheus.andrade@ccc.ufcg.edu.br>
2022-07-25 12:46:03 -0300

    Análise das rotas de add e remover

--- src/pages/spam/index.jsx
+++ src/pages/spam/index.jsx
@@ -4,12 +4,12 @@ import ReturnButton from "../../assets/icons/return-button.svg";
 import PostComponent from "../../components/Post";
 import ProfileComponent from "../../components/Profile";
 import RemoveModalComponent from "../../components/RemoveModal";
-import { getAllReports, getAllReportsByCount } from "../../services/api";
+import { getAllReports, getAllReportsByCount, reportPost, reportUser, reportTribo, ignoreReport
+,removeUser, removePost, removeTribo} from "../../services/api";
 import {PostData, ProfileData} from "../../data/Data.js";
 import { spamOptions, } from "../../util/aux";
 import Select from "../../components/Select";
 import { useEffect } from "react";
-
 
 const Spam = () => {
 
@@ -20,45 +20,132 @@ const Spam = () => {
     const [reports, setReports] = useState([]);
     const [reportsByCount, setReportsByCount] = useState([]);
 
-
     useEffect(() => { 
                
       async function getReports(){
         const reportsData = await getAllReports();
-        setReports(reportsData.reports);
+        setReports(reportsData.reports);       
       }
 
       async function getReportsByCount(){
         const reportsDataByCount = await getAllReportsByCount();
-        setReportsByCount(reportsDataByCount.reportsByCount);
+        setReportsByCount(reportsDataByCount.reports);
+        
       }
-      
-      getReportsByCount();    
-      getReports();
-    }, []);
+  
+    verifySelectedType();
+    getReportsByCount();    
+    getReports();
 
+    }, [selectedType]);
 
+    function verifySelectedType(){
+        if(selectedType === "time"){
+            setIsMostRecent(true);
+        } else{
+            setIsMostRecent(false);
+        }
+      }
+
+    const addPost = () => {
+        addReportPost();
+        console.log(reports)
+        console.log(reportsByCount)
+        console.log(selectedType)        
+    }
+    
+    async function addReportPost(){
+        await reportPost("62295255cd7d3237ba7bb8f8");            
+    }
+
+      const addUser = () => {
+          addReportUser();
+          console.log(reports)
+          console.log(reportsByCount)
+          console.log(selectedType)         
+      }
+    async function addReportUser(){
+        await reportUser("62295255cd7d3237ba7bb8f8");            
+    }
+
+      const addTribo = () => {
+          addReportTribo();
+          console.log(reports)
+          console.log(reportsByCount)
+          console.log(selectedType)         
+      }
+
+      async function addReportTribo(){
+        await reportTribo("62295255cd7d3237ba7bb8f8");            
+    }
 
     const printar = () => {
         console.log(reports)
-        console.log(reportsByCount)     
+        console.log(reportsByCount)
+        console.log(selectedType)
         
     }
 
+    const delPost = () => {
+        deletarPost();
+        console.log(reports)
+        console.log(reportsByCount)
+        console.log(selectedType)       
+    }
 
-    useEffect(() => {
-        async function showReports(){           
-        }
+    async function deletarPost(){
+        await removePost("626324ebba865e7e12cab126");            
+    }
+
+    const delUser = () => {
+        deletarUser();
+        console.log(reports)
+        console.log(reportsByCount)
+        console.log(selectedType)       
+    }
+    async function deletarUser(){
+        await removeUser("626324ebba865e7e12cab126");            
+    }
+
+    const delTribo = () => {
+        deletarTribo();
+        console.log(reports)
+        console.log(reportsByCount)
+        console.log(selectedType)       
+    }
+
+    async function deletarTribo(){
+        await removeTribo("626324ebba865e7e12cab126");            
+    }
+
+    const igReport = () => {
+        ignorarReport();
+        console.log(reports)
+        console.log(reportsByCount)
+        console.log(selectedType)       
+    }
+
+    async function ignorarReport(){
+        await ignoreReport("626324ebba865e7e12cab126");            
+    }
         
-        showReports();
-      }, []);
-  
 
     return(
   
     <div className= {styles.container}>
 
-        <button onClick={ () => printar()}> </button>
+        <div className={styles.test}>
+            <button onClick={ () => addPost()}> addPost</button>
+            <button onClick={ () => addUser()}> addUser </button>
+            <button onClick={ () => addTribo()}>  addTribo</button>
+            <button onClick={ () => printar()}>  printar</button>
+            <button onClick={ () => delPost()}>  delPost</button>
+            <button onClick={ () => delUser()}>  delUser</button>
+            <button onClick={ () => delTribo()}>  delTribo</button>
+            <button onClick={ () => igReport()}>  ignore</button>
+            
+        </div>
+
              
         <div className= {isModalVisible ? styles.titleAreaOpacity : styles.titleArea}>           
             <a href="/" className= {styles.returnButton}> <img  src= {ReturnButton} alt= "Botão de retorno" /> </a> 
@@ -114,7 +201,6 @@ const Spam = () => {
                     Time = {PostData[0].time}
                     Image = {value.reported.content[0].url}
                     Subtitle = {value.reported.text}
-                    Complaints = {value.reported.reportsQty}
                     SetValue = {setIsModalVisible}
                 /> 
                 }
@@ -124,28 +210,12 @@ const Spam = () => {
                     Icon = {value.reported.profilePic.url}
                     Time = {ProfileData[0].time}
                     Name = {value.reported.name}
-                    Complaints = {value.reported.reportsQty}
                     SetValue = {setIsModalVisible} 
                 /> 
                 }
             })
         }
-        
-
-        <PostComponent 
-            User = {reports.map((value,index) => {
-                if( index === 2){
-                    return value.reported.tribo.username;
-                }
-            })
-            }
-            Icon = {PostData[0].icon}
-            Time = {PostData[0].time}
-            Image = {PostData[0].image}
-            Subtitle = {PostData[0].subtitle}
-            Complaints = {PostData[0].complaints}
-            SetValue = {setIsModalVisible}
-        />        
+    
 
         <PostComponent 
             User ={PostData[0].user}
--- src/services/api.js
+++ src/services/api.js
@@ -51,12 +51,124 @@ export const getAllReportsByCount = async () => {
   await api
     .get("/report/count")
     .then((resp) => {
-      dataByCount = resp.dataByCount;
+      dataByCount = resp.data;
     })
     .catch((ex) => {
       dataByCount = null;
     });
   return dataByCount;
 };
+
+export const reportPost = async (id) => {
+  await api
+    .post("/report/post", 
+    {
+        "idReported": "62dd532dbc5ea46068c1763b"
+    })
+    .then((resp) => {
+      console.log(resp)
+    })
+    .catch((ex) => {  
+      console.log(ex)
+    });
+};
+
+export const removePost = async (id) => {
+  await api
+    .delete("/report/post", 
+    {
+        "idReport": "62dd50a6bc5ea46068c1758f"
+    })
+    .then((res) => {
+      console.log(res)
+    })
+    .catch((ex) => {  
+      console.log(ex)
+    });
+};
+
+export const reportUser = async (id) => {
+  await api
+    .post("/report/user", 
+    {
+        "idReported": "626324ebba865e7e12cab126"
+    })
+    .then((resp) => {
+      console.log(resp)
+    })
+    .catch((ex) => {  
+      console.log(ex)
+    });
+};
+
+export const removeUser = async (id) => {
+  await api
+    .delete("/report/user", 
+    {
+       "idReport": "62cc6815c54ae99554cd2e5b"
+    })
+    .then((res) => {
+      console.log(res)
+    })
+    .catch((ex) => {  
+      console.log(ex)
+    });
+};
+
+export const reportTribo = async (id) => {
+  await api
+    .post("/report/tribo", 
+    {
+        "idReported": "62cc6829c54ae99554cd2e61"
+    })
+    .then((resp) => {
+      console.log(resp)
+    })
+    .catch((ex) => {  
+      console.log(ex)
+    });
+};
+
+export const removeTribo = async (id) => {
+  await api
+    .delete("/report/tribo", 
+    {
+        "idReport": "62295255cd7d3237ba7bb8f8"
+    })
+    .then((res) => {
+      console.log(res)
+    })
+    .catch((ex) => {  
+      console.log(ex)
+    });
+};
+
+export const ignoreReport = async (id) => {
+  await api
+    .delete("/report/ignore", 
+    {
+        data: {"idReport": "62cc6829c54ae99554cd2e61"}
+    })
+    .then((res) => {
+      console.log(res)
+    })
+    .catch((ex) => {  
+      console.log(ex)
+    });
+};
+
+
+axios.post('/user', {
+  firstName: 'Fred',
+  lastName: 'Flintstone'
+})
+.then(function (response) {
+  console.log(response);
+})
+.catch(function (error) {
+  console.log(error);
+});
+
+
 
 
