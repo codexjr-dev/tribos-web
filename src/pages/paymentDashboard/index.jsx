7fc69ec2272339cd6ea42cb667c75502ebeca5e8
Luciano Erick Figueiredo <luciano.erick.filho@ccc.ufcg.edu.br>
2022-08-10 21:31:03 -0300

    Adicionando modal pra exibição de detalhes de pagamento

--- src/pages/paymentDashboard/index.jsx
+++ src/pages/paymentDashboard/index.jsx
@@ -7,66 +7,105 @@ import { StackedChart } from "../../components/StackedChart";
 
 import { typeLabels, intervalLabels } from "../../util/options";
 import { useNavigate } from "react-router-dom";
+import { PaymentDetails } from "../../components/paymentDetails";
+import { useState } from "react";
+import { useEffect } from "react";
 
 export const PaymentDashboard = () => {
+  const [showDetailsActive, setShowDetailsActive] = useState(false);
+  const [triboDetails, setTriboDetails] = useState(null);
+
+  const handleShowDetails = (element) => {
+    setShowDetailsActive(!showDetailsActive);
+    setTriboDetails(element);
+  };
 
   const navigate = useNavigate();
 
+  const close = () => {
+    if (showDetailsActive) {
+      setShowDetailsActive(false);
+    }
+  };
+
+  useEffect(() => {
+    console.log(showDetailsActive);
+  }, [showDetailsActive]);
+
   return (
-    <div className={styles.container}>
-      <header>
-        <div onClick={() => navigate('/dashboard')}>
-          <img src={LeftArrowIcon} alt="Voltar" />
-          <h2> Pagamentos </h2>
+    <>
+      {showDetailsActive ? (
+        <div className={styles.modalContainer}>
+          <PaymentDetails
+            handleClose={close}
+            details={triboDetails}
+          />
         </div>
-      </header>
-      <div className={styles.buttonsHeader}>
-        <img src={ConfigIcon} alt="Ajustar" onClick={() => navigate('/payment/table')}/>
-        <ButtonChain labels={intervalLabels} />
-      </div>
-      <main>
-        <div className={styles.generalInfo}>
-          <h2>Visão Geral</h2>
-          <p id={styles.amount}>R$ 2754.75</p>
-          <div className={styles.chart}>
-            <StackedChart data={generalFinances} />
+      ) : null}
+      <div className={styles.container}>
+        <header>
+          <div onClick={() => navigate("/dashboard")}>
+            <img src={LeftArrowIcon} alt="Voltar" />
+            <h2> Pagamentos </h2>
           </div>
+        </header>
+        <div className={styles.buttonsHeader}>
+          <img
+            src={ConfigIcon}
+            alt="Ajustar"
+            onClick={() => navigate("/payment/table")}
+          />
+          <ButtonChain labels={intervalLabels} />
         </div>
-        <div className={styles.paymentList}>
-          <div className={styles.tableHeader}>
-            <div className={styles.buttons}>
-              <h2>Listagem</h2>
-              <ButtonChain labels={typeLabels} />
+        <main>
+          <div className={styles.generalInfo}>
+            <h2>Visão Geral</h2>
+            <p id={styles.amount}>R$ 2754.75</p>
+            <div className={styles.chart}>
+              <StackedChart data={generalFinances} />
             </div>
           </div>
-          <div className={styles.tableContainer}>
-            <table>
-              <thead>
-                <tr>
-                  <th>Nome Tribo</th>
-                  <th>Valor total</th>
-                  <th>Imposto</th>
-                  <th>Cacique</th>
-                  <th>Tribo Master</th>
-                </tr>
-              </thead>
-              <tbody>
-                {paymentListData.map((element) => {
-                  return (
-                    <tr key={element.id} id={element.id} className={styles.tableRow}>
-                      <td>{element.name}</td>
-                      <td>{element.value}</td>
-                      <td>{element.tax}</td>
-                      <td>{element.common}</td>
-                      <td>{element.master}</td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
+          <div className={styles.paymentList}>
+            <div className={styles.tableHeader}>
+              <div className={styles.buttons}>
+                <h2>Listagem</h2>
+                <ButtonChain labels={typeLabels} />
+              </div>
+            </div>
+            <div className={styles.tableContainer}>
+              <table>
+                <thead>
+                  <tr>
+                    <th>Nome Tribo</th>
+                    <th>Valor total</th>
+                    <th>Imposto</th>
+                    <th>Cacique</th>
+                    <th>Tribo Master</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {paymentListData.map((element) => {
+                    return (
+                      <tr
+                        key={element.id}
+                        id={element.id}
+                        className={styles.tableRow}
+                        onClick={() => handleShowDetails(element)}
+                      >
+                        <td>{element.name}</td>
+                        <td>{element.value}</td>
+                        <td>{element.tax}</td>
+                        <td>{element.common}</td>
+                        <td>{element.master}</td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
           </div>
-        </div>
-      </main>
-    </div>
+        </main>
+      </div>
+    </>
   );
 };
