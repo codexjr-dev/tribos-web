96226d3c2269670c23d7cc9263d80cc48809ab9d
Luciano Erick Figueiredo <luciano.erick.filho@ccc.ufcg.edu.br>
2022-10-01 10:16:06 -0300

    Adicionando botão de feed

--- src/components/ButtonChain/index.jsx
+++ src/components/ButtonChain/index.jsx
@@ -2,11 +2,14 @@ import styles from "./styles.module.css";
 
 import { useState } from "react";
 
-export const ButtonChain = ({ labels }) => {
+export const ButtonChain = ({ labels, selected }) => {
   const [selectedIndex, setSelectedIndex] = useState(0);
+
   const handleSelect = (currentIndex) => {
     setSelectedIndex(currentIndex);
-  };
+    selected(selectedIndex);
+  }
+
   return (
     <div className={styles.container}>
       {labels.map((label, index) => {
@@ -16,7 +19,10 @@ export const ButtonChain = ({ labels }) => {
             className={
               label.id === selectedIndex ? styles.selected : styles.normal
             }
-            onClick={(e) => handleSelect(index)}
+            onClick={(e) => {
+              console.log(index);
+              return handleSelect(index);
+            }}
           >
             {label.name}
           </button>
