a7c8d089496ff3502938297c01764b3aba1b29ea
igoramf <igoraaf1@gmail.com>
2023-12-13 17:09:20 -0300

    feat: next 6 months button

--- src/components/ButtonChain/index.jsx
+++ src/components/ButtonChain/index.jsx
@@ -6,10 +6,12 @@ import Flatpickr from "react-flatpickr";
 import "flatpickr/dist/themes/light.css";
 import Portuguese from 'flatpickr/dist/l10n/pt';
 import 'flatpickr/dist/l10n/pt'; 
+import { useRef } from "react";
 
 export const ButtonChain = ({ labels, selected, calendar }) => {
   const [selectedIndex, setSelectedIndex] = useState(0);
   const [datas, setDatas] = useState([]);
+  const flatpickrRef = useRef(null);
 
   const handleSelect = (currentIndex) => {
     setSelectedIndex(currentIndex);
@@ -19,6 +21,27 @@ export const ButtonChain = ({ labels, selected, calendar }) => {
   const handleDatas = (event) => {
     setDatas(event)
   }
+
+
+  const handleSelectNext6Months = () => {
+    const currentDate = new Date();
+    const next6Months = new Date(currentDate);
+    next6Months.setMonth(next6Months.getMonth() + 6);
+    setDatas([currentDate, next6Months]);
+
+
+  };
+
+  const handleFlatpickrReady = () => {
+    const button = document.createElement('button');
+    button.innerHTML = 'Selecionar Próximos 6 Meses';
+    button.classList.add('next-6-months-button');
+    button.addEventListener('click', handleSelectNext6Months);
+    button.style.backgroundColor = 'white';
+
+    const calendarContainer = document.querySelector('.flatpickr-calendar');
+    calendarContainer.appendChild(button);
+  };
 
   return (
     <div className={styles.container}>
@@ -41,12 +64,15 @@ export const ButtonChain = ({ labels, selected, calendar }) => {
         <div className={styles.calendarContainer}>
             <Flatpickr className={styles.calendarInput} 
               placeholder="Informe uma data"
+              value={ datas.length > 0 ? datas : [new Date()]}
+              ref={flatpickrRef}
               options={{
                 mode: "range",
                 dateFormat: "d/m/Y",
                 minDate: "today",
                 locale: Portuguese.pt,
-                onClose: (e) => handleDatas(e)
+                onClose: (e) => handleDatas(e),
+                onReady: handleFlatpickrReady
               }
               }>
             </Flatpickr>
