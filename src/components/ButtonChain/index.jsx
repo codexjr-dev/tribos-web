4e7c3336be5a082b4c877a4062e46f3bc80438f1
igoramf <igoraaf1@gmail.com>
2023-12-13 16:47:19 -0300

    feat: flatpickr calendar

--- src/components/ButtonChain/index.jsx
+++ src/components/ButtonChain/index.jsx
@@ -2,15 +2,23 @@ import styles from "./styles.module.css";
 
 import { useState } from "react";
 import { useEffect } from "react";
+import Flatpickr from "react-flatpickr";
+import "flatpickr/dist/themes/light.css";
+import Portuguese from 'flatpickr/dist/l10n/pt';
+import 'flatpickr/dist/l10n/pt'; 
 
-export const ButtonChain = ({ labels, selected }) => {
+export const ButtonChain = ({ labels, selected, calendar }) => {
   const [selectedIndex, setSelectedIndex] = useState(0);
+  const [datas, setDatas] = useState([]);
 
   const handleSelect = (currentIndex) => {
     setSelectedIndex(currentIndex);
     selected(currentIndex);
-}
+  }
 
+  const handleDatas = (event) => {
+    setDatas(event)
+  }
 
   return (
     <div className={styles.container}>
@@ -29,6 +37,24 @@ export const ButtonChain = ({ labels, selected }) => {
           </button>
         );
       })}
+      {calendar &&
+        <div className={styles.calendarContainer}>
+            <Flatpickr className={styles.calendarInput} 
+              placeholder="Informe uma data"
+              options={{
+                mode: "range",
+                dateFormat: "d/m/Y",
+                minDate: "today",
+                locale: Portuguese.pt,
+                onClose: (e) => handleDatas(e)
+              }
+              }>
+            </Flatpickr>
+            {!datas[1] &&
+              <span>Preencha a data corretamente</span>
+            }
+          </div>
+        }
     </div>
   );
 };
