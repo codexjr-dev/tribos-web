524ea541131436133a426692b66a589bb7590435
Laysa <laysa.silva@ccc.ufcg.edu.br>
2024-02-16 17:50:03 -0300

    feat: add change password

--- src/pages/ChangePassword/index.jsx
+++ src/pages/ChangePassword/index.jsx
@@ -2,70 +2,76 @@ import { React, useEffect, useState } from "react";
 import LeftArrowIcon from "../../assets/icons/left-arrow-icon.svg";
 import { useNavigate } from "react-router-dom";
 import styles from "./styles.module.css";
-import { getAllUsers, redefinePassword } from "../../services/api";
+import { useAuth } from "../../contexts/auth";
+import { changePasswordWeb } from "../../services/api";
 import { useCallback } from "react";
 
 export const ChangePassword = () => {
+  const { user } = useAuth();
+
   const navigate = useNavigate();
 
-  const [email, setEmail] = useState("");
+  const [password, setpassword] = useState("");
   const [newPassword, setnewPassword] = useState("");
   const [newPassword2, setnewPassword2] = useState("");
-  const [showPassword, setShowPassword] = useState("password");
 
-  const validateForm = useCallback(() => {
-    return email !== "" && newPassword === newPassword2;
-  }, [email, newPassword, newPassword2]);
-
-  const handleClick = useCallback(() => {
-    if (validateForm()) {
-      redefinePassword(email, newPassword);
+  async function handleClick() {
+    if (newPassword !== newPassword2) {
+      alert("As novas senhas não são iguais. Por favor, tente novamente.");
     } else {
-      alert("Por favor, verifique suas informações.");
+      alert("senha alterada com sucesso");
+      changePasswordWeb(password, user._id, newPassword);
+      navigate("/dashboard/users/day");
     }
-  }, [email, newPassword, validateForm]);
+  }
 
   return (
     <div className={styles.container}>
       <header
         style={{ marginBottom: "3rem", display: "flex", alignItems: "center" }}
       >
-        <div onClick={() => navigate("/dashboard/day")}>
+        <div onClick={() => navigate(-1)}>
           <img src={LeftArrowIcon} alt="Voltar" />
           <h2> Alterar Senha </h2>
         </div>
       </header>
       <div>
-        <p>Email</p>
+        <p>Senha atual</p>
         <input
-          type="name"
-          placeholder="Email"
+          type="password"
+          placeholder="senha atual"
           className={styles.userInput}
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
+          value={password}
+          onChange={(e) => setpassword(e.target.value)}
         />
+      </div>
+      <div>
         <p>Nova senha</p>
         <input
-          type={showPassword}
-          placeholder="Nova senha"
+          type="password"
+          placeholder="digite a nova senha"
           className={styles.userInput}
-          setpassword={showPassword}
           value={newPassword}
           onChange={(e) => setnewPassword(e.target.value)}
         />
+      </div>
+      <div>
         <p>Confirmar senha</p>
         <input
-          type={showPassword}
-          placeholder="Digite novamente a senha"
+          type="password"
+          placeholder="digite novamente a senha"
           className={styles.userInput}
-          setpassword={showPassword}
           value={newPassword2}
           onChange={(e) => setnewPassword2(e.target.value)}
         />
       </div>
       <div style={{ padding: 30 }}>
-        <button disabled={!validateForm()} onClick={handleClick}>
-          Salvar
+        <button
+          onClick={async () => {
+            await handleClick();
+          }}
+        >
+          Enviar
         </button>
       </div>
     </div>
