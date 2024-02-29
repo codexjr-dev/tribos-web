3ade55490696a2e3246ee36c4ab16c5afe5bea58
igoramf <igoraaf1@gmail.com>
2024-02-29 19:39:44 -0300

    trocando a url da api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:4444/",
+  baseURL: "https://tribos.herokuapp.com",
 });
 
 api.defaults.headers.common["Authorization"] = `Bearer ${localStorage.getItem(
