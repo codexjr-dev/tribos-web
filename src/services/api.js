1dd7196bd67ba4acfe2c7d1af0e134b708d311fc
Henriqueplp <henriqueleonhpp@outlook.com>
2024-12-04 19:35:28 -0300

    fix: endereço de back-end alterado

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://tribos.herokuapp.com",
+  baseURL: process.env.REACT_APP_API_LOCAL,
 });
 
 api.defaults.headers.common["Authorization"] = `Bearer ${localStorage.getItem(
