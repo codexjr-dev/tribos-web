8523df25fbefee07ddfa5ca84d372c2d71b788b9
PedroFDiniz <63296960+PedroFDiniz@users.noreply.github.com>
2024-12-09 20:49:08 -0300

    fix: alterado endereço do backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://tribos.herokuapp.com",
+  baseURL: "https://tribos-backend.onrender.com",
 });
 
 api.defaults.headers.common["Authorization"] = `Bearer ${localStorage.getItem(
